fa71f3a5f25bb327a3e6f25adf4c4c114234d7ed
thuyetpham <pvthuyet@gmail.com>
2020-12-24 17:16:45 +0700

    attribute_watcher.cpp security_watcher.cpp folder_name_watcher.cpp

--- FileWatcherDemo/file_activity/attribute_watcher.cpp
+++ FileWatcherDemo/file_activity/attribute_watcher.cpp
@@ -5,8 +5,17 @@ namespace died
 {
 	void attribute_watcher::do_notify(file_notify_info info)
 	{
-		SPDLOG_INFO(L"{} - {}", info.get_action(), info.get_path_wstring());
-		mModel.push(std::move(info));
+		switch (info.get_action())
+		{
+		case FILE_ACTION_MODIFIED:
+			SPDLOG_INFO(L"{} - {}", info.get_action(), info.get_path_wstring());
+			mModel.push(std::move(info));
+			break;
+
+		default:
+			SPDLOG_INFO(L"Ignore: {} - {}", info.get_action(), info.get_path_wstring());
+			break;
+		}
 	}
 
 	model_file_info& attribute_watcher::get_model()
--- FileWatcherDemo/file_activity/folder_name_watcher.cpp
+++ FileWatcherDemo/file_activity/folder_name_watcher.cpp
@@ -5,8 +5,17 @@ namespace died
 {
 	void folder_name_watcher::do_notify(file_notify_info info)
 	{
-		SPDLOG_INFO(L"{} - {}", info.get_action(), info.get_path_wstring());
-		mModel.push(std::move(info));
+		switch (info.get_action())
+		{
+		case FILE_ACTION_REMOVED:
+			SPDLOG_INFO(L"{} - {}", info.get_action(), info.get_path_wstring());
+			mModel.push(std::move(info));
+			break;
+
+		default:
+			SPDLOG_INFO(L"Ignore: {} - {}", info.get_action(), info.get_path_wstring());
+			break;
+		}
 	}
 
 	model_file_info& folder_name_watcher::get_model()
